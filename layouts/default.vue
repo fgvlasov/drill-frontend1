899a47fbc1c2eff47343a77a6b2cb78f587f34ee
”Aleksandr <aleksandr.tskm@gmail.com>
2023-05-18 15:15:27 +0300

    Made header styles in default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,49 +1,47 @@
 <template>
   <header>
-
+    <div>
       <div class="submenu">
-        <NuxtLink to="/">Компания</NuxtLink>
-        <NuxtLink to="completedobjects">Выполненные объекты</NuxtLink>
-        <NuxtLink to="fotoandvideo">Фото и видео</NuxtLink>
-        <NuxtLink to="workingconditions">Условия работы</NuxtLink>
-        <NuxtLink to="articles">Статьи</NuxtLink>
-        <NuxtLink to="reviews">Отзывы</NuxtLink>
-        <NuxtLink to="contacts">Контакты</NuxtLink>
-	  </div>
+        <NuxtLink to="/" class="item-menu">Компания</NuxtLink>
+        <NuxtLink to="completedobjects" class="item-menu">Выполненные объекты</NuxtLink>
+        <NuxtLink to="fotoandvideo" class="item-menu">Фото и видео</NuxtLink>
+        <NuxtLink to="workingconditions" class="item-menu">Условия работы</NuxtLink>
+        <NuxtLink to="articles" class="item-menu">Статьи</NuxtLink>
+        <NuxtLink to="reviews" class="item-menu">Отзывы</NuxtLink>
+        <NuxtLink to="contacts" class="item-menu">Контакты</NuxtLink>
+      </div>
 
       <div class="social-icons">
-        <NuxtLink to="https://www.instagram.com/concut_ru/">Instagram</NuxtLink>
-        <NuxtLink to="http://www.youtube.com/channel/UCftRGdsIOSOzVS-5LsuMy9A/feed?activity_view=3">Youtube</NuxtLink>
-        <NuxtLink to="http://vk.com/concut">Vk</NuxtLink>
-	  </div>
-
-	  <div>
+        <NuxtLink to="https://www.instagram.com/concut_ru/" class="instagram"></NuxtLink>
+        <NuxtLink to="http://www.youtube.com/channel/UCftRGdsIOSOzVS-5LsuMy9A/feed?activity_view=3" class="youtube"></NuxtLink>
+        <NuxtLink to="http://vk.com/concut" class="vk"></NuxtLink>
+      </div>
+    </div>
+	  <div class="branding">
       <div>
-        <NuxtLink to="https://concut.ru">
+        <NuxtLink to="https://concut.ru" class="item-menu">
           <img src="~assets/images/logo.png" alt="Concut" />
         </NuxtLink>
       </div>
-      <div>
-        <div>
+      <div  class="item-select">
+        <div class="phone-wrapper">
           <NuxtLink :to="'tel: ' + activeIdx.phone">{{ activeIdx.extended_phone }}</NuxtLink>
           <NuxtLink to="contacts">Как нас найти</NuxtLink>
         </div>
-        <div>
-            <select v-model="activeIndex">
-              <option  v-for="(reg, idx) in regional_phones" v-bind:value="idx">
-                {{ reg.name_region }}
-              </option>
-            </select>
-        </div>
+        <select v-model="activeIndex">
+          <option  v-for="(reg, idx) in regional_phones" v-bind:value="idx">
+            {{ reg.name_region }}
+          </option>
+        </select>
       </div>
     </div>
-    <div>
-      <nav class="">
-        <NuxtLink to="drill">АЛМАЗНОЕ СВЕРЛЕНИЕ</NuxtLink>
-        <NuxtLink to="cut">АЛМАЗНАЯ РЕЗКА</NuxtLink>
-        <NuxtLink to="dismantling">ДЕМОНТАЖ</NuxtLink>
-        <NuxtLink to="groove">ШТРОБЛЕНИЕ</NuxtLink>
-        <NuxtLink to="grind">ШЛИФОВКА</NuxtLink>
+    <div class="menu-leaf">
+      <nav>
+        <NuxtLink to="drill" class="item-menu">АЛМАЗНОЕ СВЕРЛЕНИЕ</NuxtLink>
+        <NuxtLink to="cut" class="item-menu">АЛМАЗНАЯ РЕЗКА</NuxtLink>
+        <NuxtLink to="dismantling" class="item-menu">ДЕМОНТАЖ</NuxtLink>
+        <NuxtLink to="groove" class="item-menu">ШТРОБЛЕНИЕ</NuxtLink>
+        <NuxtLink to="grind" class="item-menu">ШЛИФОВКА</NuxtLink>
       </nav>
     </div>
   </header>
@@ -78,4 +76,50 @@ export default {
 
 <style scoped>
 
+.submenu {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+}
+
+.item-menu {
+    padding: 5px;
+}
+.social-icons {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: auto;
+}
+.branding {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+}
+.item-select {
+    margin-left: auto;
+    display: flex;
+    /*justify-content: flex-start;*/
+    justify-content: space-evenly;
+    align-items: center;
+    /*flex-flow: column wrap;*/
+}
+.select-index {
+    margin-left: auto;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-wrap: nowrap;
+}
+.phone-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: unsafe center;
+    padding: 5px;
+}
+.menu-leaf {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	css: [
-		// SCSS file in the project
-		'@/assets/scss/app.scss'
-	  ]
+    css: [
+        // CSS file in the project
+        // '@/assets/css/app.css',
+        // SCSS file in the project
+        '@/assets/scss/app.scss'
+    ]
 })
