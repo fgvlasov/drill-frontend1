143302acb3644b43b5cd0a85c5afeb79b8efe284
fgvlasov <fgvlasov@gmail.com>
2023-05-22 13:53:38 +0300

    fetching data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	css: [
+	modules: ['@nuxtjs/strapi'],
+  	strapi: {
+    	//url: process.env.STRAPI_URL || 'http://localhost:1337',
+		url: process.env.STRAPI_URL || 'https://drill.ptzsite.ru',
+  		prefix: '/api',
+  		version: 'v4',
+  		cookie: {},
+  		cookieName: 'strapi_jwt'
+  	},
+	  css: [
 		// SCSS file in the project
 		'@/assets/scss/app.scss'
-	  ]
+	  ],
 })
--- pages/cut.vue
+++ pages/cut.vue
@@ -1,11 +1,35 @@
 <template>
 <p>Алмазная резка</p>
+{{ console.log(works) }}
 </template>
 
 <script>
 export default {
-  name: "cut"
-}
+        // use destructuring to get the $strapi instance from context object
+        async asyncData({ $strapi }) {
+          try {
+            // fetch data from strapi
+            const works = await (
+              await fetch(`${store.state.apiUrl}/works?populate=*`)
+            ).json()
+            const services = await (
+              await fetch(`${store.state.apiUrl}/services?populate=*`)
+            ).json()
+            
+            // make the fetched data available in the page
+            // also, return the .data property of the entities where
+            // the data we need is stored
+            return {
+			  works: works.data,
+              services: services.data,
+			  
+            }
+			
+          } catch (error) {
+            console.log(error)
+          }
+        },
+      }
 </script>
 
 <style scoped>
